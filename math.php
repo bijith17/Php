6bee083bca9125c0f0e9d9da0d4ae83cdc92f830
Bijith <118971527+bijith17@users.noreply.github.com>
2022-12-14 13:02:46 +0530

    Add files via upload

---
+++ math.php
@@ -0,0 +1,22 @@
+<?php
+
+// pi() function returns the value
+echo(pi()); // returns 3.1415926535898
+
+// min() and max() functions can be used to find the lowest or highest value in a list of argument
+echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+
+// abs() function returns the absolute (positive) value of a number
+echo(abs(-6.7));  // returns 6.7
+
+// sqrt() function returns the square root of a number
+echo(sqrt(64));  // returns 8
+
+// round() function rounds a floating-point number to its nearest integer
+echo(round(0.60));  // returns 1
+echo(round(0.49));  // returns 0
+
+// rand() function generates a random number
+echo(rand());
+?>
---
+++ strings.php
@@ -0,0 +1,18 @@
+<?php
+// strlen() function returns the length of a string.
+echo strlen("Hello world!"); // outputs 12
+
+// str_word_count() function counts the number of words in a string.
+echo str_word_count("Hello world!"); // outputs 2
+
+// strrev() function reverses a string.
+echo strrev("Hello world!"); // outputs !dlrow olleH
+
+// strpos() function searches for a specific text within a string
+echo strpos("Hello world!", "world"); // outputs 6
+
+// str_replace() function replaces some characters with some other characters in a string.
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly! 
+
+
+?>
